c349602d07031bdba0afb07d9d51d464e8026dc6
avirozen <117524971+avirozen@users.noreply.github.com>
2023-11-19 18:05:03 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
-input.onGesture(Gesture.Shake, function () {
-    led.toggle(x, y)
-    x = randint(0, 4)
-    y = randint(0, 4)
-    led.toggle(x, y)
-})
-let y = 0
-let x = 0
-x = 2
-y = 2
+let x = 2
+let y = 2
 led.toggle(x, y)
+let Counter = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        led.toggle(x, y)
+        x = randint(0, 4)
+        y = randint(0, 4)
+        Counter += 1
+        led.toggle(x, y)
+        basic.pause(1000)
+        if (x == 2 && x == 2) {
+            basic.showNumber(Counter)
+            basic.pause(1000)
+            game.gameOver()
+        }
+    }
 })
